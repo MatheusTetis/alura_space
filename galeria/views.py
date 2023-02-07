aa89860e2b372d83d0b160b87286361886133e83
MatheusTetis <eng.matheuspt@gmail.com>
2023-02-07 19:28:01 -0300

    Aula 6

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(
+        request,
+        template_name='galeria/index.html',
+        context={'numero_de_cards': [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]}
+    )
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
