df5fcdd194e4d8a009dc645c21a58dd35aceac8d
MatheusTetis <eng.matheuspt@gmail.com>
2023-02-08 23:22:06 -0300

    Aula 7

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Fotografia(models.Model):
+    # Colunas ou atributos da tabela
+    # Especificando que o atributo nome deve ter no máximo
+    # 100 caracteres e que não pode ser nulo nem vazio
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f'Fotografia [nome={self.nome}]'
--- galeria/views.py
+++ galeria/views.py
@@ -1,12 +1,86 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from galeria.models import Fotografia
+
 
 # Create your views here.
 def index(request):
+    dados = {
+        1: {
+            "nome": "Nebulosa de Carina",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        2: {
+            "nome": "Galáxia NGC 1079",
+            "legenda": "nasa.org / NASA / Hubble"
+        },
+        3: {
+            "nome": "Núvem Molecular do Camaleão",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        4: {
+            "nome": "NGC 346",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        5: {
+            "nome": "JWST Pesquisa do Campo Profundo Extragalático (JADES)",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        6: {
+            "nome": "Nebulosa do Anel do Sul",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        7: {
+            "nome": "Pilares da Criação",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        8: {
+            "nome": "L1527 e Proporestrela",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        9: {
+            "nome": "Close de Netuno",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        10: {
+            "nome": "Nebulosa da Tarântula",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        11: {
+            "nome": "Galáxia Roda de Carro",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        12: {
+            "nome": "Penhasco Cósmico",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        13: {
+            "nome": "Quinteto de Stephan",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        14: {
+            "nome": "Primeiro Campo Profundo do James Webb",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+        15: {
+            "nome": "Visão Multiondas da NGC 1300",
+            "legenda": "webbtelescope.org / NASA / James Webb"
+        },
+    }
+
+    fotografias = Fotografia.objects.all()
+
     return render(
         request,
         template_name='galeria/index.html',
-        context={'numero_de_cards': [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]}
+        context={'cards': fotografias}
     )
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    # pk = Primary Key
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+
+    return render(
+        request,
+        template_name='galeria/imagem.html',
+        context={'fotografia': fotografia}
+    )
