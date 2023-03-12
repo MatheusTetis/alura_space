534d4c24c31e486f79c6366b4eefae8dc8b00b8c
MatheusTetis <eng.matheuspt@gmail.com>
2023-03-12 17:38:58 -0300

    media url

--- setup/urls.py
+++ setup/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
     path('', include('usuarios.urls')),
-] + staticfiles_urlpatterns() #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
